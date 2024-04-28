6557906c46ae57e77865299fbb2e494b9d0155cd
= <reynolds552@gmail.com>
2024-04-27 17:36:56 -0700

    Textures for the asteroids

--- cubie/AsteroidsState.gd
+++ cubie/AsteroidsState.gd
@@ -1,10 +1,27 @@
 class_name AsteroidsState extends CubieState
 
+var materials = []
+
+func _init():
+	var texture_paths = [
+		"res://assets/textures/500megapack/coffeestain.jpg",
+		"res://assets/textures/500megapack/fuzzygrid.jpg",
+		"res://assets/textures/500megapack/hardmicrogrommed.jpg",
+		"res://assets/textures/500megapack/rustedspoon.jpg",
+		"res://assets/textures/500megapack/specklemarginator.jpg",
+		"res://assets/textures/500megapack/staticmoss.jpg"
+	]
+	for path in texture_paths:
+		var texture = load(path)
+		var material = StandardMaterial3D.new()
+		material.albedo_texture = texture
+		materials.append(material)
+
 func enter(cubie: Node3D):
-	var num_asteroids = randf_range(5, 20)  # Random number of asteroids
+	var num_asteroids = randf_range(5, 20)
 	for i in range(int(num_asteroids)):
 		var sphere = SphereMesh.new()
-		var size = randf_range(10, 50)  # Random size
+		var size = randf_range(10, 50)
 		sphere.radius = size
 		sphere.height = size * 2
 
@@ -15,10 +32,12 @@ func enter(cubie: Node3D):
 			randf_range(-500 + size, 500 - size),
 			randf_range(-500 + size, 500 - size)
 		)
+
+		instance.material_override = materials[randi() % materials.size()]
+
 		cubie.add_child(instance)
 
 func exit(cubie: Node3D):
-	# Remove all asteroid instances when exiting this state
 	for child in cubie.get_children():
 		if child is MeshInstance3D:
 			child.queue_free()
