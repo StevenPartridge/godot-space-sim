c38c0a499c1bfee0e840ab15cda8d17f7dce3666
= <reynolds552@gmail.com>
2024-04-27 17:31:26 -0700

    Much better controls

--- SpaceShip.gd
+++ SpaceShip.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-@export var max_speed = 50.0
+@export var max_speed = 100.0
 @export var acceleration = 0.6
 @export var pitch_speed = 1.5
 @export var roll_speed = 1.9
-@export var input_response = 8.0
+@export var input_response = 4.0
 
 var forward_speed = 0.0
 var pitch_input = 0.0
@@ -29,6 +29,13 @@ func get_input(delta):
 		roll_input = lerp(roll_input, 1.0, input_response * delta)
 	else:
 		roll_input = lerp(roll_input, 0.0, input_response * delta)
+		
+	if Input.is_action_pressed("turn_right"):
+		yaw_input = lerp(yaw_input, -0.1, input_response * delta)
+	elif Input.is_action_pressed("turn_left"):
+		yaw_input = lerp(yaw_input, 0.1, input_response * delta)
+	else:
+		yaw_input = lerp(yaw_input, 0.0, input_response * delta)
 
 func _physics_process(delta):
 	get_input(delta)
@@ -36,6 +43,8 @@ func _physics_process(delta):
 			roll_input * roll_speed * delta)
 	transform.basis = transform.basis.rotated(transform.basis.x,
 			pitch_input * pitch_speed * delta)
+	transform.basis = transform.basis.rotated(transform.basis.y,
+			yaw_input * roll_speed * delta)
 	transform.basis = transform.basis.orthonormalized()
 	velocity = -transform.basis.z * forward_speed
 	move_and_collide(velocity * delta)
